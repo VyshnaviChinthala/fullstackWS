0203f5f3dca82ad8daa525c12a4e99d0e06724a2
VyshnaviChinthala <vyshnavichinthala36@gmail.com>
2024-10-26 17:41:21 +0530

    day 5

---
+++ ws/React/sample-project2/src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar"
+
+
+
+const App = () => {
+    const data={
+        appname:"Hello"
+    }
+    const logodata ="mylogo"
+    const test ="hello world"
+  return (
+       <>
+       <p className="text-yellow-500">{test}</p>
+            <Navbar appdata={data} logo={logodata}/> 
+        </>
+  )
+}
+
+export default App
+
