7d08268f2fab9aada6502adae9f39964e5f4b58a
VyshnaviChinthala <vyshnavichinthala36@gmail.com>
2024-10-27 22:11:50 +0530

    d6 final

--- ws/React/route-project/src/App.jsx
+++ ws/React/route-project/src/App.jsx
@@ -3,15 +3,18 @@ import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
+import WebLayout from './layout/WebLayout'
 const App = () => {
   return (
     <>
     <BrowserRouter>
     <Routes>
+    <Route element={<WebLayout />}>
           <Route path='/' element={<Home/>}/>
           <Route path='/product' element ={<Product/>}/>
           <Route path='/contact' element={<Contact/>}/>
-    </Routes>
+          </Route>
+          </Routes>
     </BrowserRouter>
     </>
   )
